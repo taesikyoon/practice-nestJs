4f40d8e38f9cb20c7d7e79af16634bc1dbbeb4da
윤태식 <taesikyoon@yuntaesig-ui-MacBookAir.local>
2022-11-15 20:02:15 +0900

    NestJS와 DB연결하기, 환경 변수 설정

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
   const PORT = process.env.PORT;
   await app.listen(PORT, () => {
     console.log('Running Server');
   });
 }
+
 bootstrap();
